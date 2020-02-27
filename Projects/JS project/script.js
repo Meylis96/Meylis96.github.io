49a7b56937e5a0d22bff9fb45484a11fe9d1e73d
Meylis96 <60135023+Meylis96@users.noreply.github.com>
2020-02-27 03:53:46 +0300

    Add files via upload

---
+++ Projects/JS project/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+a = 5,
+cosnole.log(a);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+
+// console.log(second);
+
+// let answer = prompt("Are you 18 year old?", "Yes");
+// console.log(typeof(null));
+// console.log(4 + 4 * 5);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);    
